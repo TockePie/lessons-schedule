2b77a626e8efa8c3923ac1b9bd6223e42f083a5c
TockePie <144056146+TockePie@users.noreply.github.com>
2024-05-05 09:00:55 +0300

    v2.1.1 Small fix with dark mode

--- src/components/lessons-table/LessonsTable.jsx
+++ src/components/lessons-table/LessonsTable.jsx
@@ -5,10 +5,12 @@ import DaysOfWeek from "./DaysOfWeek.jsx";
 import TableBody from "./TableBody.jsx";
 
 export default function Main(props) {
+  const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
   return (
     <div style={{ overflow: "hidden" }}>
       <div className="table-bg">
-        <Table striped bordered data-bs-theme="dark">
+        <Table striped bordered data-bs-theme={isDarkMode ? "dark" : "light"}>
           <DaysOfWeek />
           <TableBody currentGroup={props.currentGroup} />
         </Table>
