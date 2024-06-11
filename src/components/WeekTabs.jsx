48da83f8ed945137c45ba8a37fa4e078a0be3ccf
TockePie <144056146+TockePie@users.noreply.github.com>
2024-06-11 16:24:05 +0300

    Chages in WeekTabs

--- src/components/WeekTabs.jsx
+++ src/components/WeekTabs.jsx
@@ -1,7 +1,6 @@
 import { useContext, useCallback } from "react";
-import { Tab, Tabs } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 
-import checkWeek from "../utils/checkWeek";
 import { ManualScheduleContext } from "../context/ManualScheduleContext";
 import { LessonsExamContext } from "../context/LessonsExamsContext";
 
@@ -9,23 +8,23 @@ export default function WeekTabs() {
   const { setIsManualWeek } = useContext(ManualScheduleContext);
   const { selectedTabKey } = useContext(LessonsExamContext);
 
-  const handleSelectionChange = useCallback(
-    (key) => {
-      setIsManualWeek(key === "even");
-    },
-    [setIsManualWeek]
-  );
+  const handleSelectionChange = useCallback(() => {
+    setIsManualWeek((prev) => !prev);
+  }, [setIsManualWeek]);
 
   if (selectedTabKey === "exams") return null;
 
   return (
-    <Tabs
-      className="flex items-center justify-center mt-3"
-      defaultSelectedKey={checkWeek() ? "odd" : "even"}
-      onSelectionChange={handleSelectionChange}
-    >
-      <Tab title="Парний" key="even" />
-      <Tab title="Непарний" key="odd" />
-    </Tabs>
+    // <Tabs
+    //   className="flex items-center justify-center mt-3"
+    //   defaultSelectedKey={checkWeek() ? "odd" : "even"}
+    //   onSelectionChange={handleSelectionChange}
+    // >
+    //   <Tab title="Парний" key="even" />
+    //   <Tab title="Непарний" key="odd" />
+    // </Tabs>
+    <div className="flex items-center justify-center mt-3">
+      <Button onClick={handleSelectionChange}>Інший тиждень</Button>
+    </div>
   );
 }
